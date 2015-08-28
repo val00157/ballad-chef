2230c98c4d9dfe93882304a025f4aea196f0ca9c
val00157 <itabashi@val.co.jp>
2015-08-28 12:01:13 +0900

    OpsWorksエージェントがmonitd.confを書き換えてしまい既存のconfファイルが読み込めないのでmonitrcファイルとしてシンボリックリンクを作成するシェルを追加

---
+++ base/recipes/ballad-config.rb
@@ -0,0 +1,14 @@
+# OpsWorksエージェントがmonitd.confを書き換えてしまい
+# 既存のconfファイルが読み込めないのでmonitrcファイルとしてシンボリックリンクを作成する。
+bash "monit_config" do
+  code <<-EOL
+    for filename in *; do
+      extension=${filename##*.}
+      if [ $extension != "monitrc" ]; then
+        linkname=${filename%.*}.monitrc
+        ln -s ${filename} ${linkname}
+      fi
+    done
+  EOL
+end
+
