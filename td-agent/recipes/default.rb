1fcb19396f8884cd837e0a63ee1bdc91003a5426
val00157 <itabashi@val.co.jp>
2015-08-31 16:49:12 +0900

    コメントミス修正、config_expanderのインストール忘れ対応

--- td-agent/recipes/default.rb
+++ td-agent/recipes/default.rb
@@ -24,8 +24,14 @@ package "td-agent" do
   action :install
 end
 
-# elasticsearchプラグインのインストール
+# forestプラグインのインストール
 gem_package "fluent-plugin-forest" do
+  gem_binary "/opt/td-agent/embedded/bin/fluent-gem"
+  action :install
+end
+
+# config_expanderプラグインのインストール
+gem_package "fluent-plugin-config-expander" do
   gem_binary "/opt/td-agent/embedded/bin/fluent-gem"
   action :install
 end
