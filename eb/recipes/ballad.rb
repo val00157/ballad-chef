670db71616e93c8a8a0699b97671e15e3d0b8164
val00157 <itabashi@val.co.jp>
2015-09-02 16:45:05 +0900

    ebの環境変数を設定する

---
+++ eb/recipes/ballad.rb
@@ -0,0 +1,16 @@
+stack = node[:opsworks][:stack][:name] 
+params = data_bag_item("eb", stack)["eb"]
+
+bash "setEnv" do
+  user "root"
+  code <<-EOL
+    eb use params["EB_ENVIRONMENT"]
+    eb setenv AWS_ACCESS_KEY_ID=params["AWS_ACCESS_KEY_ID"]                                     \
+              AWS_SECRET_KEY=params["AWS_SECRET_KEY"]                                           \
+              AWS_PROPERTY_DOMAIN_URL=params["AWS_PROPERTY_DOMAIN_URL"]                         \
+              AWS_PROPERTY_IMAGE_SERVER_URL=params["AWS_PROPERTY_IMAGE_SERVER_URL"]             \
+              AWS_PROPERTY_STATIC_FILE_SERVER_URL=params["AWS_PROPERTY_STATIC_FILE_SERVER_URL"] \
+              AWS_PROPERTY_TAG_NAME=params["AWS_PROPERTY_TAG_NAME"]                             \
+              S3BUCKET=params["S3BUCKET"]
+  EOL
+end
